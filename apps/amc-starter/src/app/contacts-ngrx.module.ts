af684c2dd803e140e6013ceb046390e2ad13c121
Thomas Burleson <ThomasBurleson@Gmail.com>
2018-10-04 11:02:35 -1000

    initial setup
    add jest support

---
+++ apps/amc-starter/src/app/contacts-ngrx.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { storeFreeze } from 'ngrx-store-freeze';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StoreModule.forRoot(
+      {},
+      { metaReducers: !environment.production ? [storeFreeze] : [] }
+    ),
+    EffectsModule.forRoot([]),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
+  ]
+})
+export class ContactsNgRxModule {}
---
+++ apps/amc-voter/src/app/voter/store/voter-store.ts
@@ -0,0 +1,51 @@
+/**
+ * To emulate a Redux store, simply
+ * support the dispatch(), subscribe() methods and
+ * use reducers to process actions with immutable instances
+ */
+export class VoterStore {
+
+  /**
+   * Accessor to current store state
+   */
+  getState():any {
+    return this.state;
+  }
+
+  /**
+   *  Dispatch the action thru reducers to
+   *  condistionally update the state
+   */
+  dispatch(action) {
+    const newState = this.reducer(this.state, action);
+    if (newState !== this.state) {
+      this.state = newState;
+      this.listeners.forEach(notify => notify());
+    }
+  }
+
+  /**
+   * Allow views to listen for store synchronous
+   * store changes
+   */
+  subscribe(notify) {
+    this.listeners.push(notify);
+  }
+
+  /**
+   * Use the custom actions to update the counter state!
+   *
+   * @TODO - add your custom actions here!
+   */
+  protected reducer(state, action){
+    return state;
+  }
+
+  protected listeners = [];
+  protected state = {
+    counter : 0
+  };
+
+}
+
+
